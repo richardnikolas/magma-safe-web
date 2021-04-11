d9a8e35e114af74fe678ef3958b6d2e5c5ff49d0
Richard Nikolas <richardnikolas1@gmail.com>
2021-04-10 21:35:14 -0300

    [Richard] Now we can store the logged user in the local storage

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,16 @@ import { routerMiddleware } from 'connected-react-router/immutable';
 import { createBrowserHistory } from 'history';
 import thunkMiddleware from 'redux-thunk';
 import createRootReducer from './reducers';
+import { storeUser } from './middlewares';
+import loadUser from './loadUser';
 
 const history = createBrowserHistory();
 
 export default function configureAppStore() {
   return configureStore({
     reducer: createRootReducer(history),
-    middleware: [routerMiddleware(history), thunkMiddleware]
+    middleware: [routerMiddleware(history), thunkMiddleware, storeUser],
+    preloadedState: loadUser()
   });
 };
 
